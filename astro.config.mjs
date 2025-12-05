66e74d71ae80a9ee1fe94a9b70d8da66bcfeda89
wushi <wzyiec@126.com>
2025-12-05 16:50:21 +0800

    1.修改标签页，添加标签列表显示功能 2.修改导航栏，添加标签页链接 3.修改博客页，添加标签显示功能 4.添加RSS订阅功能 5.修改foolter，添加返回首页链接

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://eclectic-biscuit-bf2a22.netlify.app/'
+});
